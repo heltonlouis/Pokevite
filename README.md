74e88275909fc556b0160767a0b1d2ac4b752ddd
Helton Luis <heltonlouis@gmail.com>
2023-04-14 12:25:18 -0300

    build: criando repositorio do projeto

---
+++ README.md
@@ -0,0 +1,23 @@
+# Pokevite
+
+### Aprendizado:
+Muito show de bola este mini curso oferecido pelo Yuri. 
+Eu super recomendo o seu curso para quem queira aprender um pouco sobre Vue.js e Vite!
+
+## Como rodar projeto?
+
+```sh
+git clone https://github.com/yurimarcon/PokeVite.git
+```
+
+```sh
+cd PokeVite
+```
+
+```sh
+npm i
+```
+
+```sh
+npm run dev
+```
---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container mt-4 text-body-secondary">
+    <div class="card" style="margin-bottom: 100px">
+      <h1 class="text-center">PokeVite</h1>
+      <div class="card-body">
+        <h2>Aprendizado:</h2>
+        <p>Muito show de bola este mini curso oferecido pelo Yuri, achei interessante deixar o video dele, pois é preciso divulgar seu trabalho. 
+          Eu super recomendo quem queira aprender um pouco sobre Vue e Vite!</p>
+        <hr>
+        <h3>Tutorial</h3>
+        <p>
+          GitHub do Yuri: <a href="https://github.com/yurimarcon/Vite-pokedex" target="_blank" alt="Link do projeto no GitHub" >/Vite-pokedex</a>
+        </p>
+        <p>A playlist com o desenvolvimento passo a passo está no YouTube e te convido a acompanhar caso queira aprender ou aprimorar suas abilidades com as técnologias utilizadas.</p>
+        <img src="https://user-images.githubusercontent.com/44410208/213874225-3556aefc-cd31-428b-aa2c-787289d8e4a3.png" alt="Curso Vue 3 e Vite">
+
+        <p class="mt-2">Agradeço as pessoas que acompanharam a incentivaram o projeto!</p>
+        <p>Caso queira desenvolver acompanhando as video aulas e fique com alguma dúvida, fique a vontade para entrar em contato via e-mail.</p>
+        <p>
+          <a class="btn btn-primary" href="mailto:yuri.erik.oliveira@gmail.com">📧 yuri.erik.oliveira@gmail.com</a>
+        </p>
+        <p>Me acompanhe no Github e no Linkedin, network é muito importante para área de tecnologia!</p>
+        <a class="btn btn-primary" target="_blank" href="https://link-service.netlify.app/" alt="Minhas mídias sociais">👨‍💻 Minhas mídias sociais 🤓 ✌️</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
